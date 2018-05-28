a221b2c2c3911a154591a96522e3dc7daf4f7994
Florian MADURA <fmadura@student.42.fr>
2018-05-28 14:53:40 +0200

    macro

--- Colleen.c
+++ Colleen.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define G(x) 34,x,34,10,
+#define S(x) ,x,10
 /*
  * This is a Self Replicant
  */
@@ -7,8 +9,10 @@ int main()
 /*
  * This is an inside comment
  */
-char *x="%s%c%s%c%s%c%s%c%s%c%s%c%s%c%s%c%s%cchar *x=%c%s%c;%cchar *a=%c%s%c;%cchar *b=%c%s%c;%cchar *c=%c%s%c;%cchar *c2=%c%s%c;%cchar *d=%c%s%c;%cchar *e=%c%s%c;%cchar *f=%c%s%c;%cchar *g=%c%s%c;%cchar *h=%c%s%c;%cchar *i=%c%s%c;%c%s%c%s%c%s%c";
+char *x="%s%c%s%c%s%c%s%c%s%c%s%c%s%c%s%c%s%c%s%c%s%cchar *x=%c%s%c;%cchar *a=%c%s%c;%cchar *a2=%c%s%c;%cchar *a3=%c%s%c;%cchar *b=%c%s%c;%cchar *c=%c%s%c;%cchar *c2=%c%s%c;%cchar *d=%c%s%c;%cchar *e=%c%s%c;%cchar *f=%c%s%c;%cchar *g=%c%s%c;%cchar *h=%c%s%c;%cchar *i=%c%s%c;%c %s%c%s%c%s%c";
 char *a="#include <stdio.h>";
+char *a2="#define G(x) 34,x,34,10,";
+char *a3="#define S(x) ,x,10";
 char *b="/*";
 char *c=" * This is a Self Replicant";
 char *c2=" * This is an inside comment";
@@ -16,8 +20,8 @@ char *d=" */";
 char *e="int main()";
 char *f="{";
 char *g="}";
-char *h="printf(x,a,10,b,10,c,10,d,10,e,10,f,10,b,10,c2,10,d,10, 34,x,34,10, 34,a,34,10, 34,b,34,10, 34,c,34,10, 34,c2,34,10, 34,d,34,10, 34,e,34,10, 34,f,34,10, 34,g,34,10, 34,h,34,10, 34,i,34,10, h,10,i,10,g,10);";
+char *h="printf(x,a,10,a2,10,b,10,c,10,d,10,e,10,f,10,b,10,c2,10,d,10, G(x) G(a) G(a2) G(b) G(c) G(c2) G(d) G(e) G(f) G(g) G(h) G(i) h,10,i,10,g,10);";
 char *i="return 0;";
-printf(x,a,10,b,10,c,10,d,10,e,10,f,10,b,10,c2,10,d,10, 34,x,34,10, 34,a,34,10, 34,b,34,10, 34,c,34,10, 34,c2,34,10, 34,d,34,10, 34,e,34,10, 34,f,34,10, 34,g,34,10, 34,h,34,10, 34,i,34,10, h,10,i,10,g,10);
+printf(x S(a) S(a2) S(a3) S(b) S(c) S(d) S(e) S(f) S(b) S(c2) S(d), G(x) G(a) G(a2) G(a3) G(b) G(c) G(c2) G(d) G(e) G(f) G(g) G(h) G(i) h,10,i,10,g,10);
 return 0;
 }
