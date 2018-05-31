9dd23938446891cab018f36b45ebb15e24d05811
Florian MADURA <fmadura@student.42.fr>
2018-05-31 21:37:23 +0200

    all

--- Sully.c
+++ Sully.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(void){
 	int i = 5;
-	char *x = ("%s%c %s%c %s%d;%c");
+	char *x = ("%s%c %s%c %s%d;%c char *x=%c%s%c;%c char *a=%c%s%c;%c char *b=%c%s%c;%c char *c=%c%s%c;%c char d[50];%c char *e=%c%s%c;%c char *f=%c%s%c;%c char *g=%c%s%c;%c char *h=%c%s%c;%c %s%cSully_%%d.c%c, i);%c %s%cw%c);%c %s%c%s%c");
 	char *a = "#include <stdio.h>"; 
 	char *b = "int main(){";
 	char *c = "int i = ";
-	char *d = "FILE *fp = fopen(d, );"; 
-	char *e = "sprintf(f,\"Sully_%d.c\", i)"; 
-	char f[50];
-	char *g = "";
+	char d[50];
+	char *e = "sprintf(d,";
+	char *f = "FILE *fp = fopen(d, "; 
+	char *g = "fprintf(fp, x, a,10, b,10, c,--i,10, 34,x,34,10, 34,a,34,10, 34,b,34,10, 34,c,34,10, 10, 34,e,34,10, 34,f,34,10, 34,g,34,10, 34,h,34,10, e,34,34,10, f,34,34,10, g,10,h,10);";
 	char *h = "return 0;}";
-	FILE *fp = fopen( d, "w" );
-	sprintf(f, "Sully_%d.c", i);
-	printf(x, a,10, b,10, c,--i,10);
+	sprintf(d, "Sully_%d.c", i);
+	FILE *fp = fopen( d, "w");
+	fprintf(fp, x, a,10, b,10, c,--i,10, 34,x,34,10, 34,a,34,10, 34,b,34,10, 34,c,34,10, 10, 34,e,34,10, 34,f,34,10, 34,g,34,10, 34,h,34,10, e,34,34,10, f,34,34,10, g,10,h,10);
 	return 0;
 }
